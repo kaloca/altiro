89132c9c58dd85e3133e4aee1ead9fb63b6d5f1d
Glitch (altiro-chat-api) <none>
2023-03-14 20:48:07 +0000

    hid firebase info

--- firebase.js
+++ firebase.js
@@ -1,17 +1,6 @@
 const firebase = require("firebase/app"),
-  { getDatabase, ref, set, child, get, update, increment } = require("firebase/database");
-
-const firebaseConfig = {
-  apiKey: "AIzaSyActIEMW0zaUrxpEYe0Ij5kGt8NAfEBL6U",
-  authDomain: "altiro-chat.firebaseapp.com",
-  databaseURL: "https://altiro-chat-default-rtdb.firebaseio.com",
-  projectId: "altiro-chat",
-  storageBucket: "altiro-chat.appspot.com",
-  messagingSenderId: "739951598398",
-  appId: "1:739951598398:web:41738ba6d21f7015d96a6f",
-  measurementId: "G-4RLNJF1YDN",
-};
-
+  { getDatabase, ref, set, child, get, update, increment } = require("firebase/database"),
+  { firebaseConfig } = require('./firebase_config.js');
 
 const firebase_app = firebase.initializeApp(firebaseConfig);
 const dbRef = ref(getDatabase(firebase_app));
